9bc4b8eff4d0c26c7d79a3a236ac72afba0d4b93
Zoe Guzman <zoeguzman.ok@gmail.com>
2023-04-26 19:27:03 -0300

    version #1

---
+++ pages/index.jsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+import Illustration from '../components/Illustration';
+import styles from '../styles/HomePage.module.css';
+
+export default function HomePage() {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.background}>
+          <h1>I BUILD</h1>
+          <h1>WEBSITES</h1>
+        </div>
+        <div className={styles.foreground}>
+          <div className={styles.content}>
+            <h1 className={styles.name}>Zoe Guzman</h1>
+            <h6 className={styles.bio}>Frontend Developer</h6>
+            <Link href="/projects">
+              <button className={styles.button}>View Work</button>
+            </Link>
+            <Link href="/contact">
+              <button className={styles.outlined}>Contact Me</button>
+            </Link>
+            <Link href="https://drive.google.com/file/d/17O-oOpHlv--Iz1xj2TmwLI0Iuvx-xRV0/view?usp=sharing">
+              <a target="_blank" ><button className={styles.button}>Download Resume</button></a>
+            </Link>
+          </div>
+          <img src="https://i.ibb.co/nsxzYLX/3324343.png"  alt="perfil"/>
+         
+        </div>
+      </div>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  return {
+    props: { title: 'Home' },
+  };
+}
